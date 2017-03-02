2621074199eb3b8ae86781a2862206421c67b877
Ken Park <spark@knowru.com>
2017-03-01 17:59:16 -0800

    wrote tests for figure and blockquote

--- tests.py
+++ tests.py
@@ -10,5 +10,35 @@ class TestKnowruMarkdownTestCase(unittest.TestCase):
         result = markdown_to_html('# this is a header')
         self.assertEqual(result, '<h1>this is a header</h1>')
 
+    def test_figure_without_title_and_caption(self):
+        result = markdown_to_html("![Alt text](/path/to/img.jpg)")
+        self.assertEqual(result, """<figure>
+    <img src="/path/to/img.jpg" alt="Alt text" />
+</figure>
+""")
+
+    def test_figure_with_title_but_without_caption(self):
+        result = markdown_to_html('![Alt text](/path/to/img.jpg "Optional title")')
+        self.assertEqual(result, """<figure>
+    <img src="/path/to/img.jpg" alt="Alt text" title="Optional title" />
+</figure>
+""")
+
+    def test_figure_with_title_and_caption(self):
+        result = markdown_to_html('![Alt text](/path/to/img.jpg "Optional title" "Optional figcaption")')
+        self.assertEqual(result, """<figure>
+    <img src="/path/to/img.jpg" alt="Alt text" title="Optional title" />
+    <figcaption>Optional figcaption</figcaption>
+</figure>
+""")
+
+    def test_blockquote_with_footer_and_cite_content_and_title(self):
+        result = markdown_to_html('> some content("some footer" ["cite content" "cite title"])')
+        self.assertEqual(result, """<blockquote>
+    <p>some content</p>
+    <footer>some footer in <cite title="cite title">cite content</cite></footer>
+</blockquote>
+""")
+
 if __name__ == '__main__':
     unittest.main()
