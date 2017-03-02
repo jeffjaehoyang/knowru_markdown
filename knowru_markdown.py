7525af83e66a1203f10a6fe486f01855e84e6d6f
Ken Park <spark@knowru.com>
2017-03-01 17:47:44 -0800

    started using python markdown; passed all existing tests

--- knowru_markdown.py
+++ knowru_markdown.py
@@ -1,7 +1,5 @@
-def markdown_to_html(markdown):
-    html = unicode()
-    if markdown.startswith('#'):
-        html = '<h1>{}</h1>'.format(markdown.replace('#', '').strip())
-    else:
-        html = '<p>{}</p>'.format(markdown)
+from markdown import markdown
+
+def markdown_to_html(user_given_text):
+    html = markdown(user_given_text)
     return html
